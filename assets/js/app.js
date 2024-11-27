2e603d8cf99773086e982e335be32c63f218fa44
Larry <lawrencengugi90@gmail.com>
2024-11-27 12:31:52 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,73 @@
+// close nav-bar on nav-link click and close navbar on outside click
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    // Close navbar when a nav-link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            if (navbarCollapse.classList.contains('show')) {
+                const bsCollapse = new bootstrap.Collapse(navbarCollapse, {toggle: true});
+                bsCollapse.hide();
+            }
+        });
+    });
+
+    // Close navbar when clicking outside of it
+    document.addEventListener('click', function (event) {
+        if (
+            navbarCollapse.classList.contains('show') &&
+            !navbarCollapse.contains(event.target) &&
+            !navbarToggler.contains(event.target)
+        ) {
+            const bsCollapse = new bootstrap.Collapse(navbarCollapse, {toggle: true});
+            bsCollapse.hide();
+        }
+    });
+});
+
+$('#portfolio-carousel').owlCarousel({
+    loop: true,
+    dots: false,
+    margin: 24,
+    nav: true,
+    smartSpeed: 700,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+// REVIEWS
+$('#reviews-carousel').owlCarousel({
+    loop: true,
+    dots: false,
+    margin: 24,
+    nav: true,
+    smartSpeed: 700,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
